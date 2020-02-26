a2041c7d342154f1ffc4dac7d20a776c4eb09d5d
parkyes90 <parkyes90@gmail.com>
2020-02-26 09:49:51 +0900

    feat($ProjectFilePath): Add Favs View

--- lists/urls.py
+++ lists/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "lists"
 
-urlpatterns = [path("add/<int:room_pk>", views.save_rooms, name="save-room")]
+urlpatterns = [
+    path("toggle/<int:room_pk>", views.toggle_room, name="toggle-room"),
+    path("favs/", views.SeeFavsView.as_view(), name="see-favs"),
+]
