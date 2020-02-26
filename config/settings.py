662cd2898e683238cd421b91c2133ac4cf063c5b
parkyes90 <parkyes90@gmail.com>
2020-02-26 16:10:39 +0900

    feat($ProjectFilePath): Add storages

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 PROJECT_APPS = [
     "core",
@@ -196,6 +196,12 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 # Sentry
 
 if not DEBUG:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-parkyes90"
+    AWS_AUTO_CREATE_BUCKET = True
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
